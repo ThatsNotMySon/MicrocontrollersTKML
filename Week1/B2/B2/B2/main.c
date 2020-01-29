b14f72bc02d7d0c3548f24c0819f9e01b67ca748
Tom <tomkaas74@gmail.com>
2020-01-29 14:18:19 +0100

    B2

---
+++ Week1/B2/B2/B2/main.c
@@ -0,0 +1,18 @@
+/*
+ * B2.c
+ *
+ * Created: 29-1-2020 14:17:57
+ * Author : dkaas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
