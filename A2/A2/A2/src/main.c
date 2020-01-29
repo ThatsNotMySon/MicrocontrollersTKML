8535b523d009f1fb90c5e446a21f35663236f1c6
Tom <tomkaas74@gmail.com>
2020-01-29 12:55:31 +0100

    A2 code

---
+++ A2/A2/A2/src/main.c
@@ -0,0 +1,60 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+#include <asf.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+	
+	DDRD = 0b11111111;			// All pins PORTD are set to output 
+	
+	while (1)
+	{
+		PORTD = 0xAA;			// Write 10101010b PORTD
+		wait( 250 );				
+		PORTD = 0x55;			// Write 01010101b PORTD
+		wait( 250 );				
+	}
+
+	/* Insert application code here, after the board has been initialized. */
+}
