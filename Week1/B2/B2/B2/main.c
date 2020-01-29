fe716472992e06a9247fc9f4c991c6453087c64d
Tom <tomkaas74@gmail.com>
2020-01-29 14:21:23 +0100

    B2Edit

--- Week1/B2/B2/B2/main.c
+++ Week1/B2/B2/B2/main.c
@@ -1,18 +1,56 @@
-/*
- * B2.c
+/**
+ * \file
  *
- * Created: 29-1-2020 14:17:57
- * Author : dkaas
- */ 
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+
+#define F_CPU 8e6
 
 #include <avr/io.h>
+#include <util/delay.h>
 
-
-int main(void)
+void wait( int ms )
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
 }
 
+int main (void)
+{	
+	DDRD = 0b11111111;			// All pins PORTD are set to output 
+	
+	while (1)
+	{
+		PORTD = 0xC0;			// Write 10101010b PORTD
+		wait( 500 );				
+		PORTD = 0x00;			// Write 01010101b PORTD
+		wait( 500 );				
+	}
+}
