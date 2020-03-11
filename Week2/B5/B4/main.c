db7f8fc5761b30582f99c2fd378fa64fc1435678
Miquel de Leeuw <mjaysondeleeuw@gmail.com>
2020-03-11 21:20:34 +0100

    B5 is working now

--- Week2/B5/B4/main.c
+++ Week2/B5/B4/main.c
@@ -1,95 +1,109 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define F_CPU 8d6
+void init();
+void display_text(char *str);
+void set_cursor(int position);
 
-void wait (int ms);
-void lcd_command( unsigned char dat);
-void init_lcd( void );
-void lcd_writeChar( unsigned char dat );
-void lcd_writeLine1 ( char text1[] );
-void lcd_writeLine2 ( char text2[] );
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );
+	}
+}
 
- void wait( int ms )
- {
-	 for (int i=0; i<ms; i++)
-	 {
-		 _delay_ms( 1 );
-	 }
- }
+void display_text(char *str)
+{
+	while(*str) {
+		lcd_write_char(*str++);
+	}	
+}
+
+void lcd_e(void)
+{
+	PORTC |= (1<<3);	// E high
+	wait(1);	
+	PORTC &= ~(1<<3);  	// E low
+	wait(1);			
+}
+
+void lcd_write_char(unsigned char dat)
+{
+	PORTC = dat & 0xF0; // hoge nibble
+	PORTC = PORTC | 0x0C; // data (RS=1),
+	
+	// start (EN=1)
+	wait(1); // wait 1 ms
+	PORTC = 0x04; // stop (EN = 0)
+	PORTC = (dat & 0x0F) << 4; // lage nibble
+	PORTC = PORTC | 0x0C; // data (RS=1),
+	
+	// start (EN=1)
+	wait(1); // wait 1 ms
+	PORTC = 0x00; // stop
+	// (EN=0 RS=0)
+}
+
+void set_cursor(int position)
+{
+	PORTC = 0x80 + position;	// function set
+	lcd_e();
+}
+
+void init()
+{
+	// PORTC output mode and all low (also E and RS pin)
+	DDRC = 0xFF;
+	PORTC = 0x00;
+
+	// function set
+	PORTC = 0x20;	
+	lcd_e();
+
+	// function set
+	PORTC = 0x20;   
+	lcd_e();
+	PORTC = 0x80;
+	lcd_e();
+
+	// Display on/off control
+	PORTC = 0x00;   
+	lcd_e();
+	PORTC = 0xF0;
+	lcd_e();
+	
+	// clear screen
+	PORTC = 0x00;   // Display on/off control
+	lcd_e();
+	PORTC = 0x01;	// function set
+	lcd_e();
+
+	// Entry mode set
+	PORTC = 0x00;   
+	lcd_e();
+	PORTC = 0x60;
+	lcd_e();
+}
+
 
 int main(void)
 {
-	init_lcd();
-	wait(250);
-	
-	char sentence[10] = "a";
-	
-	lcd_writeLine1(sentence);
-	
-	return 0;
-}
+	// Init I/O
+	DDRD = 0xFF;			// PORTD(7) output, PORTD(6:0) input
 
-void lcd_writeLine1 ( char text1[] )
-{
-	// eerst de eerste 8 karakters = regel 1
-	// eerste pos regel 1
-	lcd_command(0x80);
-	for (int i=0; i<16; i++) {
-		lcd_writeChar( text1[i] );
+	// Init LCD
+	init();
+
+	// Write sample string
+	display_text("MS-DOS");
+
+	// Loop forever
+	while (1)
+	{
+		PORTD ^= (1<<7);	// Toggle PORTD.7
+		wait(250);	
 	}
-}
 
-void lcd_writeLine2 ( char text2[] )
-{
-	// dan de eerste 8 karakters = regel 2
-	// eerste pos regel 2
-	lcd_command(0xC0);
-	for (int i=0; i<16; i++) {
-		lcd_writeChar( text2[i] );
-	}
+	return 1;
 }
-
-// Initialisatie ldr
-//
-void init_lcd(void)
-{
-	// return home
-	lcd_command( 0x02 );
-	// mode: 4 bits interface data, 2 lines, 5x8 dots
-	lcd_command( 0x28 );
-	// display: on, cursor off, blinking off
-	lcd_command( 0x0C );
-	// entry mode: cursor to right, no shift
-	lcd_command( 0x06 );
-	// RAM adress: 0, first position, line 1
-	lcd_command( 0x80 );
-}
-
-void lcd_writeChar( unsigned char dat )
-{
-	PORTC = dat & 0xF0; // hoge nibble
-	PORTC = PORTC | 0x0C; // data (RS=1),
-	// start (EN=1)
-	_delay_ms(1); // wait 1 ms
-	PORTC = 0x04; // stop (EN = 0)
-	PORTC = (dat & 0x0F) << 4; // lage nibble
-	PORTC = PORTC | 0x0C; // data (RS=1),
-	// start (EN=1)
-	_delay_ms(1); // wait 1 ms
-	PORTC = 0x00; // stop
-	// (EN=0 RS=0)
-}void lcd_command ( unsigned char dat )
-{
-	PORTC = dat & 0xF0; // hoge nibble
-	PORTC = PORTC | 0x08; // data (RS=0),
-	// start (EN=1)
-	_delay_ms(1); // wait 1 ms
-	PORTC = 0x04; // stop (EN = 0)
-	PORTC = (dat & 0x0F) << 4; // lage nibble
-	PORTC = PORTC | 0x08; // data (RS=0),
-	// start (EN=1)
-	_delay_ms(1); // wait 1 ms
-	PORTC = 0x00; // stop
-	// (EN=0 RS=0)
-}
